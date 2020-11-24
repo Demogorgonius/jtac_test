7d0dda09976bf931e457eba79b86c435eb51ca65
Sergey <zvoncov@gmail.com>
2020-11-24 18:01:51 +0300

    Initial Commit

---
+++ jtac_testTests/jtac_testTests.swift
@@ -0,0 +1,33 @@
+//
+//  jtac_testTests.swift
+//  jtac_testTests
+//
+//  Created by Sergey on 24.11.2020.
+//
+
+import XCTest
+@testable import jtac_test
+
+class jtac_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
